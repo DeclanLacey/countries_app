6caf8d0e70f912dbb091ba6097f634b2fdf14a33
DeclanLacey <declan.lacey@hotmail.com>
2024-08-17 14:44:16 -0500

    changed name of country-card

---
+++ src/app/country-cards/country-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-cards.component.html',
+  styleUrl: './country-cards.component.css'
+})
+export class CountryCardsComponent {
+
+}

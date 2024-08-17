650ce50eec855ea3f120fb274f4a6cd550c98063
DeclanLacey <declan.lacey@hotmail.com>
2024-08-17 14:01:27 -0500

    Boiler plate code for created components

---
+++ src/app/searchbar-filter/searchbar-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbar-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './searchbar-filter.component.html',
+  styleUrl: './searchbar-filter.component.css'
+})
+export class SearchbarFilterComponent {
+
+}

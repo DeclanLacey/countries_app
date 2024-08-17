b70c04b1403c25b9b851ee3867ec0d123e5cd9b8
DeclanLacey <declan.lacey@hotmail.com>
2024-08-17 15:25:56 -0500

    added api call and styling for country cards

--- src/app/country-cards/country-cards.component.ts
+++ src/app/country-cards/country-cards.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-country-cards',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './country-cards.component.html',
   styleUrl: './country-cards.component.css'
 })
-export class CountryCardsComponent {
+export class CountryCardsComponent implements OnInit {
+  httpClient = inject(HttpClient)
+  allCountriesData: any[] = []
 
+  ngOnInit(): void {
+    this.fetchData()
+  }
+
+  fetchData() {
+    this.httpClient.get('https://restcountries.com/v3.1/all')
+    .subscribe((data: any) => {
+      console.log(data)
+      this.allCountriesData = data
+    })
+  }
 }

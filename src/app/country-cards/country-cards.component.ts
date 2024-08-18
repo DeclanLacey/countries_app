c7a81a29a9f0aabc3be5dd31386b0f20013fd947
DeclanLacey <declan.lacey@hotmail.com>
2024-08-18 16:28:04 -0500

    implemented ability for user to search and filter

--- src/app/country-cards/country-cards.component.ts
+++ src/app/country-cards/country-cards.component.ts
@@ -2,17 +2,21 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FilterPipeModule } from 'ngx-filter-pipe';
+
 
 @Component({
   selector: 'app-country-cards',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, FilterPipeModule],
   templateUrl: './country-cards.component.html',
   styleUrl: './country-cards.component.css'
 })
 export class CountryCardsComponent implements OnInit {
   httpClient = inject(HttpClient)
   allCountriesData: any[] = []
+  countryNameFilter: string = ""
+  countryRegionFilter: string = ''
 
   ngOnInit(): void {
     this.fetchData()
@@ -23,5 +27,15 @@ export class CountryCardsComponent implements OnInit {
     .subscribe((data: any) => {
       this.allCountriesData = data
     })
+  }
+
+  updateSearchName(event: Event) {
+    const newValue = (event.target as HTMLInputElement).value;
+    this.countryNameFilter = newValue
+  }
+
+  updateRegion(event: Event) {
+    const newValue = (event.target as HTMLInputElement).value;
+    this.countryRegionFilter = newValue
   }
 }
